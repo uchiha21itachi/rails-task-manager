192c3f5a738120b1b2af4fe8a07f08e8d0af6e75
yash sharma <yash221b@gmail.com>
2017-08-08 17:07:45 -0300

    working but looks ugly

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   root to: 'tasks#index'
+
+   get 'tasks/new', to: 'tasks#new'
+   post 'tasks', to: 'tasks#create'
+   get 'tasks', to: 'tasks#index'
+   get 'tasks/:id', to: 'tasks#show',as: "task"
+
+   get 'tasks/:id/edit', to: 'tasks#edit', as: "task_edit"
+   patch 'tasks/:id', to: 'tasks#update', as: "task_update"
+
+   delete 'tasks/:id', to: 'tasks#destroy', as:"task_delete"
+
+
+
 end
